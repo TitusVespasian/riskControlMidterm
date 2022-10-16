060263676e34c620cbf7b52d1bdef8de257696f9
TitusVespasian <18141155687@163.com>
2022-10-16 20:05:34 +0800

    file added

--- master_feature_selection.py
+++ master_feature_selection.py
@@ -6,34 +6,30 @@ Created on Sun Oct  9 21:22:13 2022
 @author: xzytql,zlwtql
 """
 
+import matplotlib.pylab as plt
 # %% import
 import pandas as pd
-import numpy as np
 import xgboost as xgb
-from xgboost.sklearn import XGBClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import cross_validate
-
-from sklearn import metrics
-import matplotlib.pylab as plt
-import re
-
 from matplotlib.pylab import rcParams
-rcParams['figure.figsize'] = 8, 6
-#from matplotlib.font_manager import FontProperties
-#myfont = FontProperties(fname=r"external-libraries/yahei.ttf",size=12)
+from sklearn import metrics
+from xgboost.sklearn import XGBClassifier
+
+rcParams['figure.figsize'] = 8, 4
+# from matplotlib.font_manager import FontProperties
+# myfont = FontProperties(fname=r"external-libraries/yahei.ttf",size=12)
 rcParams['font.sans-serif'] = ['SimHei']
 
 
 # %% 特征分析函数
 def getFeaturename(name):
     try:
-        rex_str=name[name.rfind('_')+1:]
+        rex_str = name[name.rfind('_') + 1:]
     except:
         print('Not successful')
-        print('name:'+name)
+        print('name:' + name)
         return name
     return rex_str
+
 
 def modelfit(alg, dtrain, y_train, dtest=None, useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
     if useTrainCV:
@@ -53,25 +49,26 @@ def modelfit(alg, dtrain, y_train, dtest=None, useTrainCV=True, cv_folds=5, earl
     # 输出模型的一些结果
     # print(dtrain_predictions)
     # print(alg.predict_proba(dtrain.as_matrix()[: ,1:]))
-    print(cvresult.shape[0])
+    if useTrainCV:
+        print(cvresult.shape[0])
     print("\n关于现在这个模型")
     print("准确率 : %.4g" % metrics.accuracy_score(y_train, dtrain_predictions))
     print("AUC 得分 (训练集): %f" % metrics.roc_auc_score(y_train, dtrain_predprob))
 
     feat_imp = pd.Series(alg.get_booster().get_fscore()).sort_values(ascending=False)
-    feat_list=[]
-    for i in range(min(len(feat_imp),20)):
-        temp_str=getFeaturename(dtrain.columns[int(feat_imp.index[i][1:])])
+    feat_list = []
+    for i in range(min(len(feat_imp), 20)):
+        temp_str = getFeaturename(dtrain.columns[int(feat_imp.index[i][1:])])
         print(temp_str, feat_imp[i])
         feat_list.append(temp_str)
-    feat_imp_new=pd.DataFrame(data=feat_list,columns=['feature_name'],index=feat_imp.index)
-    feat_temp = pd.DataFrame(data=feat_imp,columns=['feature_score'])
+    feat_imp_new = pd.DataFrame(data=feat_list, columns=['feature_name'], index=feat_imp.index)
+    feat_temp = pd.DataFrame(data=feat_imp, columns=['feature_score'])
     feat_imp_new = pd.concat([feat_imp_new, feat_temp], axis=1)
     print(feat_imp.shape)
-    feat_imp_new.plot(x='feature_name',y='feature_score',kind='barh',
+    feat_imp_new.plot(x='feature_name', y='feature_score', kind='barh',
                       title='Feature Importances')
     plt.ylabel('Feature Importance Score')
-    #plt.gcf().subplots_adjust(left=0.2)
+    # plt.gcf().subplots_adjust(left=0.2)
     plt.show()
 
 
@@ -89,8 +86,6 @@ xgb1 = XGBClassifier(
     seed=27
 )
 
-
-
 # %% 开始分析特征
 
 train_master = pd.read_csv(r"./data/train/Master_Training_Cleaned.csv")
@@ -99,8 +94,8 @@ y_train = train_master["target"].values
 # 'UserInfo_9'
 print(train_master['UserInfo_9'].unique())
 # ['中国移动' '中国电信' '不详' '中国联通']
-train_master=train_master.join(pd.get_dummies(train_master.UserInfo_9,prefix="UserInfo_9"))
-train_master.drop('UserInfo_9',axis=1,inplace=True)
+train_master = train_master.join(pd.get_dummies(train_master.UserInfo_9, prefix="UserInfo_9"))
+train_master.drop('UserInfo_9', axis=1, inplace=True)
 try:
     print(train_master.UserInfo_9)
 except AttributeError:
@@ -120,9 +115,9 @@ D     27867
 初婚        1
 Name: UserInfo_22, dtype: int64
 """
-train_master.loc[train_master['UserInfo_22']=='初婚','UserInfo_22']="已婚"
-train_master=train_master.join(pd.get_dummies(train_master.UserInfo_22,prefix="UserInfo_22"))
-train_master.drop('UserInfo_22',axis=1,inplace=True)
+train_master.loc[train_master['UserInfo_22'] == '初婚', 'UserInfo_22'] = "已婚"
+train_master = train_master.join(pd.get_dummies(train_master.UserInfo_22, prefix="UserInfo_22"))
+train_master.drop('UserInfo_22', axis=1, inplace=True)
 try:
     print(train_master.UserInfo_9)
 except AttributeError:
@@ -130,11 +125,12 @@ except AttributeError:
 
 # 'UserInfo_23'
 print(train_master['UserInfo_23'].value_counts())
-dummies_UserInfo_23=pd.get_dummies(train_master['UserInfo_23'], prefix='UserInfo_23')
+dummies_UserInfo_23 = pd.get_dummies(train_master['UserInfo_23'], prefix='UserInfo_23')
 modelfit(xgb1, dummies_UserInfo_23, y_train)
+
+# 'UserInfo_
 
 dummies_UserInfo_2 = pd.get_dummies(train_master['UserInfo_2'], prefix='UserInfo_2')
 
-
-#modelfit(xgb1, dummies_UserInfo_2, y_train)
+# modelfit(xgb1, dummies_UserInfo_2, y_train)
 # modelfit(xgb1, dummies_UserInfo_2, y_train)
