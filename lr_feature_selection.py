eee9c8ed9d7091bc7c4350715ad0b330aa808ed7
TitusVespasian <18141155687@163.com>
2022-10-17 14:31:56 +0800

    lr_feature_selection_finished
    lr_feature_selection_finished
    xgboost_feature_selection may warrant changes to reduce the dimesion of the one hot factors depending on the training results.

--- lr_feature_selection.py
+++ lr_feature_selection.py
@@ -21,40 +21,44 @@ rcParams['font.sans-serif'] = ['SimHei']
 
 
 # %% 特征分析函数
-def getFeaturename(name):
+def get_featurename(name):
     try:
         rex_str = name[name.rfind('_') + 1:]
-    except:
+    except Exception as err:
+        print(err)
         print('Not successful')
         print('name:' + name)
         return name
     return rex_str
 
-def plot_haves(train_master,feature_name,have_char):
-    target_have = train_master.target[train_master[feature_name] == have_char]\
+
+def plot_haves(_train_master, feature_name, have_char):
+    target_have = _train_master.target[_train_master[feature_name] == have_char] \
         .value_counts()
-    target_not_have = train_master.target[train_master[feature_name] != have_char]\
+    target_not_have = _train_master.target[_train_master[feature_name] != have_char] \
         .value_counts()
     df_WeblogInfo_20 = pd.DataFrame({'no_have': target_have, 'have': target_not_have})
     df_WeblogInfo_20.plot(kind='bar', stacked=True)
-    plt.title(u'有无'+feature_name+'对结果的影响')
+    plt.title(u'有无' + feature_name + '对结果的影响')
     plt.xlabel(u'有无')
     plt.ylabel(u'违约情况')
-    plt.savefig('./backup/'+feature_name+'_hnh.jpg')
+    plt.savefig('./backup/' + feature_name + '_hnh.jpg')
     plt.show()
     plt.pause(6)  # 间隔的秒数：6s
     plt.close()
 
-def modelfit(alg, dtrain, y_train, dtest=None, useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
+
+def modelfit(alg, dtrain, _y_train, dtest=None, useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
+    cvresult = 0
     if useTrainCV:
         xgb_param = alg.get_xgb_params()
-        xgtrain = xgb.DMatrix(dtrain.values[:, :], label=y_train)
+        xgtrain = xgb.DMatrix(dtrain.values[:, :], label=_y_train)
         cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], nfold=cv_folds,
                           early_stopping_rounds=early_stopping_rounds)
         alg.set_params(n_estimators=cvresult.shape[0])
 
     # 建模
-    alg.fit(dtrain.values[:, :], y_train, eval_metric='auc')
+    alg.fit(dtrain.values[:, :], _y_train, eval_metric='auc')
 
     # 对训练集预测
     dtrain_predictions = alg.predict(dtrain.values[:, :])
@@ -66,25 +70,25 @@ def modelfit(alg, dtrain, y_train, dtest=None, useTrainCV=True, cv_folds=5, earl
     if useTrainCV:
         print(cvresult.shape[0])
     print("\n关于现在这个模型")
-    print("准确率 : %.4g" % metrics.accuracy_score(y_train, dtrain_predictions))
-    print("AUC 得分 (训练集): %f" % metrics.roc_auc_score(y_train, dtrain_predprob))
+    print("准确率 : %.4g" % metrics.accuracy_score(_y_train, dtrain_predictions))
+    print("AUC 得分 (训练集): %f" % metrics.roc_auc_score(_y_train, dtrain_predprob))
 
     feat_imp = pd.Series(alg.get_booster().get_fscore()).sort_values(ascending=False)
     feat_list = []
-    for i in range(min(len(feat_imp), 20)):
-        temp_str = getFeaturename(dtrain.columns[int(feat_imp.index[i][1:])])
-        print(temp_str, feat_imp[i])
+    for i in range(len(feat_imp)):
+        temp_str = get_featurename(dtrain.columns[int(feat_imp.index[i][1:])])
+        if i < 20:
+            print(temp_str, feat_imp[i])
         feat_list.append(temp_str)
     feat_imp_new = pd.DataFrame(data=feat_list, columns=['feature_name'], index=feat_imp.index)
     feat_temp = pd.DataFrame(data=feat_imp, columns=['feature_score'])
     feat_imp_new = pd.concat([feat_imp_new, feat_temp], axis=1)
-    print(feat_imp.shape)
+    print(feat_imp_new.shape)
     feat_imp_new.plot(x='feature_name', y='feature_score', kind='barh',
                       title='Feature Importances')
     plt.ylabel('Feature Importance Score')
     # plt.gcf().subplots_adjust(left=0.2)
     plt.show()
-
 
 
 xgb1 = XGBClassifier(
@@ -153,17 +157,62 @@ True     27867
 False     2133
 Name: UserInfo_24, dtype: int64
 """
-plot_haves(train_master,'UserInfo_24','D')
-temp=train_master['UserInfo_24'].copy()
-train_master['UserInfo_24']=temp.apply(lambda x : 1 if x=='D' else 0)
+plot_haves(train_master, 'UserInfo_24', 'D')
+temp = train_master['UserInfo_24'].copy()
+train_master['UserInfo_24'] = temp.apply(lambda x: 1 if x == 'D' else 0)
 
-rest_col=['Education_Info2','Education_Info3','Education_Info4','Education_Info6','Education_Info7',
-          'Education_Info8','WeblogInfo_19','WeblogInfo_20','WeblogInfo_21']
-for i in rest_col:
-    print(train_master[rest_col].value_counts())
+rest_col = ['Education_Info2', 'Education_Info3', 'Education_Info4', 'Education_Info6', 'Education_Info7',
+            'Education_Info8', 'WeblogInfo_19', 'WeblogInfo_20', 'WeblogInfo_21']
 
+# 'Education_Info2'
+plot_haves(train_master, 'Education_Info2', 'E')
+temp = train_master['Education_Info2'].copy()
+train_master['Education_Info2'] = temp.apply(lambda x: 1 if x == 'E' else 0)
 
+# 'Education_Info3'
+print(train_master['Education_Info3'].value_counts())
+# len=3
+train_master = train_master.join(pd.get_dummies(train_master.Education_Info3, prefix="Education_Info3"))
+train_master.drop('Education_Info3', axis=1, inplace=True)
 
+# 'Education_Info4'
+print(train_master['Education_Info4'].value_counts())
+# len=6
+train_master = train_master.join(pd.get_dummies(train_master.Education_Info4, prefix="Education_Info4"))
+train_master.drop('Education_Info4', axis=1, inplace=True)
+
+# 'Education_Info6'
+print(train_master['Education_Info6'].value_counts())
+# len=6
+train_master = train_master.join(pd.get_dummies(train_master.Education_Info6, prefix="Education_Info6"))
+train_master.drop('Education_Info6', axis=1, inplace=True)
+
+# 'Education_Info7'
+print(train_master['Education_Info7'].value_counts())
+temp = train_master['Education_Info7'].copy()
+train_master['Education_Info7'] = temp.apply(lambda x: 1 if x == 'E' else 0)
+
+# 'Education_Info8'
+print(train_master['Education_Info8'].value_counts())
+# len=7
+train_master = train_master.join(pd.get_dummies(train_master.Education_Info8, prefix="Education_Info8"))
+train_master.drop('Education_Info8', axis=1, inplace=True)
+
+# 'WeblogInfo_19'
+print(train_master['WeblogInfo_19'].value_counts())
+# len=7
+train_master = train_master.join(pd.get_dummies(train_master.WeblogInfo_19, prefix="WeblogInfo_19"))
+train_master.drop('WeblogInfo_19', axis=1, inplace=True)
+
+# WeblogInfo_20
+# len=20
+# modelfit(xgb1, dummies_WeblogInfo_20, y_train)
+train_master = train_master.join(pd.get_dummies(train_master.WeblogInfo_20, prefix="WeblogInfo_20"))
+train_master.drop('WeblogInfo_20', axis=1, inplace=True)
+
+# WeblogInfo_21
+train_master = train_master.join(pd.get_dummies(train_master.WeblogInfo_21, prefix="WeblogInfo_21"))
+train_master.drop('WeblogInfo_21', axis=1, inplace=True)
 
 dummies_UserInfo_2 = pd.get_dummies(train_master['UserInfo_2'], prefix='UserInfo_2')
 
