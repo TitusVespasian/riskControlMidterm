cdccf86468a9a1743bdca8f1db5a7a1d5536824a
TitusVespasian <18141155687@163.com>
2022-10-20 19:59:09 +0800

    small change

--- lightGBM_training.py
+++ lightGBM_training.py
@@ -17,8 +17,8 @@ train_master = city_and_province_process.city_process(train_master)
 
 train_all = merge_data.merge_data(train_master, update_info_pd, log_info_pd)
 
-X = train_master.drop(['target'], axis=1)  # .to_numpy()
-y = train_master.target  # .to_numpy()
+X = train_all.drop(['target'], axis=1)  # .to_numpy()
+y = train_all.target  # .to_numpy()
 # new_dict = {key:i for (i,key) in enumerate(X.columns)}
 categorical_ordered = ['UserInfo_1', 'UserInfo_3', 'UserInfo_5', 'UserInfo_6', 'UserInfo_14', 'UserInfo_15',
                        'UserInfo_16', 'SocialNetwork_1', 'SocialNetwork_2', 'SocialNetwork_7', 'SocialNetwork_12']
@@ -164,14 +164,15 @@ best_pa= {'n_estimators': 73, 'max_depth': 7, 'num_leaves': 15, 'max_bin': 65, '
 # %% 第七步：降低学习率，增加迭代次数，验证模型
 from sklearn import metrics
 
-model = lgb.LGBMClassifier(boosting_type='gbdt', objective='binary', metrics='auc', learning_rate=0.01,
-                           n_estimators=1000, max_depth=best_pa['max_depth'],
+model = lgb.LGBMClassifier(boosting_type='gbdt', objective='binary', metrics='auc', learning_rate=0.1,
+                           max_depth=best_pa['max_depth'],
                            num_leaves=best_pa['num_leaves'], max_bin=best_pa['max_bin'],
                            min_data_in_leaf=best_pa['min_data_in_leaf'],
                            bagging_fraction=best_pa['bagging_fraction'], bagging_freq=best_pa['bagging_freq'],
                            feature_fraction=best_pa['feature_fraction'],
                            lambda_l1=best_pa['lambda_l1'], lambda_l2=best_pa['lambda_l2'],
-                           min_split_gain=best_pa['min_split_gain'], is_unbalance=True)
+                           min_split_gain=best_pa['min_split_gain'],
+                           n_estimators=73, is_unbalance=True)
 model.fit(X_train, y_train)
 import joblib
 joblib.dump(model, 'dota_model.pkl')
@@ -190,51 +191,3 @@ print("auc:", metrics.roc_auc_score(y_test, y_pre))
 confusion_matrix_result = metrics.confusion_matrix(y_test,y_pre)
 print('The confusion matrix result:\n',confusion_matrix_result)
 
-def temp():
-    train_master = pd.read_csv('./data/train/Master_Training_Cleaned.csv')
-    test_master = city_and_province_process.city_process(train_master)
-    update_info_pd = pd.read_csv('data/train/userupdate_df.csv')
-    log_info_pd = pd.read_csv('data/train/loginfo_df.csv')
-
-    test_master=pd.read_csv('./data/test/Master_Test_Cleaned.csv')
-    test_master=city_and_province_process.city_process(test_master)
-    test_update_info_pd = pd.read_csv('data/test/test_userupdate_df.csv')
-    test_log_info_pd=pd.read_csv('./data/test/test_loginfo_df.csv')
-
-    train_all = merge_data.merge_data(train_master, update_info_pd, log_info_pd)
-    test_all=merge_data.merge_data(test_master,test_log_info_pd,test_log_info_pd)
-
-    train_all=train_all.drop(['target'], axis=1)
-
-    X = train_master.drop(['target'], axis=1)  # .to_numpy()
-    y = train_master.target  # .to_numpy()
-    # new_dict = {key:i for (i,key) in enumerate(X.columns)}
-    categorical_ordered = ['UserInfo_1', 'UserInfo_3', 'UserInfo_5', 'UserInfo_6', 'UserInfo_14', 'UserInfo_15',
-                           'UserInfo_16', 'SocialNetwork_1', 'SocialNetwork_2', 'SocialNetwork_7', 'SocialNetwork_12']
-    categorical_cols = ['UserInfo_2_level', 'UserInfo_4_level', 'UserInfo_6', 'UserInfo_7', 'UserInfo_8_level',
-                        'UserInfo_9',
-                        'UserInfo_11', 'UserInfo_12', 'UserInfo_13', 'UserInfo_17', 'UserInfo_19', 'UserInfo_20_level',
-                        'UserInfo_21', 'UserInfo_22', 'UserInfo_23', 'UserInfo_24', 'Education_Info1',
-                        'Education_Info2',
-                        'Education_Info3', 'Education_Info4', 'Education_Info5', 'Education_Info6', 'Education_Info7',
-                        'Education_Info8',
-                        'WeblogInfo_19', 'WeblogInfo_20', 'WeblogInfo_21', 'city_sim']
-
-    for icol in categorical_ordered:
-        X[icol].astype('category').cat.as_ordered()
-
-    for icol in categorical_cols:
-        X[icol].astype('category')
-
-    new_dict = {key: re.sub('[^A-Za-z0-9]+', '', key) + str(i) for (i, key) in enumerate(X.columns)}
-    X.rename(columns=new_dict, inplace=True)
-
-    list_fea_str = ['UserInfo720', 'UserInfo921', 'UserInfo1931', 'UserInfo2233', 'UserInfo2334', 'UserInfo2435',
-                    'EducationInfo237', 'EducationInfo338', 'EducationInfo439', 'EducationInfo641', 'EducationInfo742',
-                    'EducationInfo843', 'WeblogInfo1944', 'WeblogInfo2045', 'WeblogInfo2146']
-    dict_fea_encode = {}
-    for icol in list_fea_str:
-        dict_fea_encode[icol] = preprocessing.LabelEncoder()
-        X[icol] = dict_fea_encode[icol].fit_transform(X[icol].astype(str))  # 将提示的包含错误数据类型这一列进行转换
-
-    X = X.drop(['Idx0'], axis=1)
